e5db58e00ed1cfa1a30cd72153679a4efc8d1e1d
Vivekanand Maurya <55177800+vivekMaury@users.noreply.github.com>
2021-07-05 14:04:38 +0530

    Add files via upload

--- Even_Odd.cpp
+++ Even_Odd.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
 int main()
 {
-  std::cout << "Hello World";
+    printf("Hello World");
+
+    return 0;
 }
